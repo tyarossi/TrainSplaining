a63ecf9734afeb91c4dee70ed3c467d968763e4a
CawsualCrow <25thunderbirdiv@gmail.com>
2025-04-04 11:41:13 -0400

    update dumb landpage mistake, confirms tailwind

--- frontend/src/components/pages/landingPage.js
+++ frontend/src/components/pages/landingPage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 
-// THIS PAGE WILL BE USED ONLY FOR TESTING CONNECTION TO MONGO. NO LONGER HOME PAGE.
 const Landingpage = () => {
     
     return (
@@ -9,9 +8,9 @@ const Landingpage = () => {
             <Card style={{ width: '30rem' }} className="mx-2 my-2">
                 <Card.Body>
                     <Card.Title>Mongo connect goodly</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Wowie zowie, the DB is connected!</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">Wowie zowie</Card.Subtitle>
                     <Card.Text>
-                        If you see this with a blue background and white text, the database is conected.
+                        If you see this with a blue background and white text, Tailwind is working.
                     </Card.Text>
                 </Card.Body>
             </Card>
