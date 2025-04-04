e911397b0453a1350be2d6be9de87d03759530dc
CawsualCrow <25thunderbirdiv@gmail.com>
2025-04-04 11:41:52 -0400

    tailwind check page

--- frontend/src/components/pages/landingPage.js
+++ frontend/src/components/pages/landingPage.js
@@ -7,7 +7,7 @@ const Landingpage = () => {
         <div className="bg-blue-500 text-white p-5">
             <Card style={{ width: '30rem' }} className="mx-2 my-2">
                 <Card.Body>
-                    <Card.Title>Mongo connect goodly</Card.Title>
+                    <Card.Title>Tailwind work</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">Wowie zowie</Card.Subtitle>
                     <Card.Text>
                         If you see this with a blue background and white text, Tailwind is working.
