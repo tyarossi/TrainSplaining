015649d510f82a41a301dc3d0b3672b4ea76fe65
tyarossi <tyarossi@gmail.com>
2025-04-04 12:36:25 -0400

    Updating, start of movement of all navbar to dropdown -Thomas

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -24,7 +24,7 @@ export default function Navbar() {
   return (
     <ReactNavbar bg="dark" variant="dark">
     <Container>
-      <Nav className="me-auto">
+      <Nav className="me-auto" style={{paddingRight: '10px'}}>
         <NavDropdown title="Schedule" id="basic-nav-dropdown">
           <Dropdown>
             <Dropdown.Toggle variant="light" id="dropdown-basic" style={{width: '100%'}}>
@@ -32,21 +32,21 @@ export default function Navbar() {
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Blue Line</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Red Line</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Green Line</Dropdown.Item>
-              <Dropdown.Item href="#/action-4">Orange Line</Dropdown.Item>
+              <Dropdown.Item href="/mbtaSchedule?Blue">Blue Line</Dropdown.Item>
+              <Dropdown.Item href="/mbtaSchedule?Red">Red Line</Dropdown.Item>
+              <Dropdown.Item href="/mbtaSchedule?Green">Green Line</Dropdown.Item>
+              <Dropdown.Item href="/mbtaSchedule?Orange">Orange Line</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
           <NavDropdown.Divider />
           <NavDropdown.Item href="/privateUserProfile">Purchase Tickets</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.3">Station Tickets</NavDropdown.Item>
+          <NavDropdown.Item href="/">Start</NavDropdown.Item>
+          <NavDropdown.Item href="/home">Home</NavDropdown.Item>
+          <NavDropdown.Item href="/privateUserProfile">Profile</NavDropdown.Item>
+          <NavDropdown.Item href="/mbtaAlerts">Alerts</NavDropdown.Item>
         </NavDropdown>
-        <Nav.Link href="/">Start</Nav.Link>
-        <Nav.Link href="/home">Home</Nav.Link>
-        <Nav.Link href="/privateUserProfile">Profile</Nav.Link>
         <Nav.Link href="/TailwindConnectionTest">Tailwind Connection Test</Nav.Link>
-
       </Nav>
     </Container>
   </ReactNavbar>
