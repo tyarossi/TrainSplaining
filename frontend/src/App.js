b9a659b2958e6e77f4be6843adb8c6b86aa207d6
Vincent Tran <hiimvincent22@gmail.com>
2025-04-09 13:18:57 -0400

    Merged all 4 pages into 1, allowed the user to change the schedule based on the included dropdown.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,10 +11,7 @@ import HomePage from "./components/pages/homePage";
 import Login from "./components/pages/loginPage";
 import Signup from "./components/pages/registerPage";
 import PrivateUserProfile from "./components/pages/privateUserProfilePage";
-import BlueStops from "./components/pages/mbtaScheduleBlue";
-import GreenStops from "./components/pages/mbtaScheduleGreen";
-import RedStops from "./components/pages/mbtaScheduleRed";
-import OrangeStops from "./components/pages/mbtaScheduleOrange";
+import Stops from "./components/pages/mbtaSchedule";
 import { createContext, useState, useEffect } from "react";
 import getUserInfo from "./utilities/decodeJwt";
 
@@ -37,10 +34,7 @@ const App = () => {
           <Route exact path="/home" element={<HomePage />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/mbtaScheduleBlueLine" element={<BlueStops />} />
-          <Route exact path="/mbtaScheduleRedLine" element={<RedStops />} />
-          <Route exact path="/mbtaScheduleGreenLine" element={<GreenStops />} />
-          <Route exact path="/mbtaScheduleOrangeLine" element={<OrangeStops />} />
+          <Route exact path="/mbtaSchedule" element={<Stops />} />
           <Route path="/privateUserProfile" element={<PrivateUserProfile />} />
           <Route path="/DatabaseConnectionTest" element={<LandingPage />} />
 
