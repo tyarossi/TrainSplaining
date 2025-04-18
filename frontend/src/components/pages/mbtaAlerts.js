97fa14e9ed8799d351b7518ffd76ec6cf9be65e9
CawsualCrow <25thunderbirdiv@gmail.com>
2025-04-18 13:15:31 -0400

    alerts now on navbar. alert card format needs fix

--- frontend/src/components/pages/mbtaAlerts.js
+++ frontend/src/components/pages/mbtaAlerts.js
@@ -2,8 +2,22 @@ import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import axios from 'axios';
 
-const PRIMARY_COLOR = "#808080";
+const PRIMARY_COLOR = "#dddddd";
 const SECONDARY_COLOR = '#404040';
+
+let realStyling = {
+  color: PRIMARY_COLOR,
+  fontWeight: "bold",
+  textDecoration: "none",
+  background: SECONDARY_COLOR,
+};
+let cardStyling = {
+  color: PRIMARY_COLOR,
+  fontWeight: "bold",
+  textDecoration: "none",
+  background: SECONDARY_COLOR,
+  width: "30rem",
+}
 
 function Alerts() {
   const [alerts, setAlerts] = useState([]);
@@ -12,7 +26,7 @@ function Alerts() {
   useEffect(() => {
     async function fetchData() {
       const result = await axios(
-        'https://api-v3.mbta.com/alerts?sort=banner&filter%5Bactivity%5D=BOARD%2CEXIT%2CRIDE',
+        'https://api-v3.mbta.com/alerts?filter%5Bactivity%5D=BOARD%2CEXIT%2CRIDE',
       );
       setAlerts(result.data.data);
     }
@@ -21,28 +35,36 @@ function Alerts() {
 
 
 return (
-  <div>
+  <div
+  style={realStyling}>
     {alerts.map(alert => (
       <Card
-      body
-      outline
-      color="success"
-      className="mx-1 my-2"
-      style={{ width: "30rem" }}
+      // body
+      // outline
+      // color="success"
+      // className="mx-1 my-2"
+      style={cardStyling}
     >
-      <Card.Body>
-      <Card.Title>Alert</Card.Title>
-      <Card.Text>{alert.attributes.header}{alert.attributes.description}</Card.Text>
+      <Card.Body
+      style={cardStyling}>
+      <Card.Title
+      style={cardStyling}>Alert</Card.Title>
+      <Card.Text
+      style={cardStyling}>{alert.attributes.header}{alert.attributes.description}</Card.Text>
       </Card.Body>
     </Card>
     ))}
 
 
-      <h1>Alerts!</h1>
+      <h1
+      style={realStyling}>Alerts!</h1>
     {alerts.map(alert => (
-      <div key={alert.id}>
-        <h3>{alert.attributes.header}</h3>
-        <p>{alert.attributes.description}</p>
+      <div key={alert.id}
+      style={realStyling}>
+        <h3
+        style={realStyling}>{alert.attributes.header}</h3>
+        <p
+        style={realStyling}>{alert.attributes.description}</p>
       </div>
     ))}
   </div>
