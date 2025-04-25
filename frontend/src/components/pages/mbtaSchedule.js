eb378bed776cb70f2a014bfb746e111868d9031f
CawsualCrow <25thunderbirdiv@gmail.com>
2025-04-25 12:54:38 -0400

    test git push new

--- frontend/src/components/pages/mbtaSchedule.js
+++ frontend/src/components/pages/mbtaSchedule.js
@@ -172,8 +172,8 @@ function MBTASchedule(){
                         <Card.Title style={realStyling}>Line Arrival and Departure Times</Card.Title>
                         <Card.Text style={realStyling}>
                         
-                            Arrival: {Date(stop.attributes.arrival_time).toLocaleString("en-US", { timeZone: "America/New_York" }) || "Not Available"}<br/>
-                            Departure: {Date(stop.attributes.departure_time).toLocaleString("en-US", { timeZone: "America/New_York" }) || "Not Available"}
+                        Arrival: {Date(stop.attributes.arrival_time).toLocaleString("en-US", { timeZone: "America/New_York" }) || "Not Available"}<br/>
+                        Departure: {Date(stop.attributes.departure_time).toLocaleString("en-US", { timeZone: "America/New_York" }) || "Not Available"}
                         </Card.Text>
                     </Card.Body>
                 </Card>
@@ -191,3 +191,5 @@ function MBTASchedule(){
 
 }
 export default MBTASchedule;
+
+// test git push  
